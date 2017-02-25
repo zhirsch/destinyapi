7e03a73199f046e2a5cea9848fcea33f65b2f0ea
Zachary Hirsch <zhirsch@umich.edu>
2017-02-25 08:33:25 -0500

    Code dump.

---
+++ vendors.go
@@ -0,0 +1,29 @@
+package destinyapi
+
+const (
+	BountyTrackerVendorHash     = 1527174714
+	Cayde6                      = 3003633346
+	CrucibleVendorHash          = 3658200622
+	CryptarchVendorHash         = 4269570979
+	DeadOrbitVendorHash         = 3611686524
+	EmblemKioskVendorHash       = 3301500998
+	EmoteKioskVendorHash        = 614738178
+	ErisMornVendorHash          = 174528503
+	EvaLevanteVendorHash        = 134701236
+	EververseVendorHash         = 3917130357
+	ExoticArmorKioskVendorHash  = 3902439767
+	ExoticWeaponKioskVendorHash = 1460182514
+	FutureWarCultVendorHash     = 1821699360
+	GunsmithVendorHash          = 570929315
+	IkoraReyVendorHash          = 1575820975
+	NewMonarchyVendorHash       = 1808244981
+	PostmasterVendorHash        = 2021251983
+	ShaderKioskVendorHash       = 2420628997
+	ShaxxVendorHash             = 3746647075
+	ShipKioskVendorHash         = 2244880194
+	ShipwrightVendorHash        = 459708109
+	SparrowKioskVendorHash      = 44395194
+	TheSpeakerVendorHash        = 2680694281
+	VanguardVendorHash          = 2668878854
+	ZavallaVendorHash           = 1990950
+)
